a627737161b2b42251c82eb931d12aba757e2928
kevchen22 <kchen012197@gmail.com>
2022-07-12 17:18:45 -0400

    final

--- src/components/SimilarExercises.js
+++ src/components/SimilarExercises.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect }from 'react';
 import { Box, Stack, Typography } from '@mui/material'
 import HorizontalScrollbar from './HorizontalScrollbar';
 import Loader from './Loader';
 
 const SimilarExercises = ({ targetMuscleExercises, equipmentExercises }) => {
+
     return (
         <Box sx={{ mt: { lg: '100px', xs: '0' }}}>
             <Typography variant='h3' mb={4} color='#fff0d4'>Exercises that target the same muscle group</Typography>
